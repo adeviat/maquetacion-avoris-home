562e060a3028849efd7727c3c5433d940e1ff8c6
Andrés Devia <43211173+adeviat@users.noreply.github.com>
2025-11-28 18:22:05 +0100

    Estructura de componentes iniciales y ruteado de home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/organisms/header/header.component";
+import { HeroComponent } from "../../components/organisms/hero/hero.component";
+import { FiltersComponent } from '../../components/organisms/filters/filters.component';
+import { TripGridComponent } from '../../components/organisms/trip-grid/trip-grid.component';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    HeaderComponent, 
+    HeroComponent,
+    FiltersComponent,
+    TripGridComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
