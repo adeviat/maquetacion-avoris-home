2548fe31ad259cd4e253b264012ed3ac3a320acf
Andrés Devia <43211173+adeviat@users.noreply.github.com>
2025-11-29 13:46:28 +0100

    Add variables Globales y Semanticas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from "../../components/organisms/header/header.compon
 import { HeroComponent } from "../../components/organisms/hero/hero.component";
 import { FiltersComponent } from '../../components/organisms/filters/filters.component';
 import { TripGridComponent } from '../../components/organisms/trip-grid/trip-grid.component';
+import { FooterComponent } from '../../components/organisms/footer/footer.component';
 
 
 @Component({
@@ -13,6 +14,7 @@ import { TripGridComponent } from '../../components/organisms/trip-grid/trip-gri
     HeroComponent,
     FiltersComponent,
     TripGridComponent,
+    FooterComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
