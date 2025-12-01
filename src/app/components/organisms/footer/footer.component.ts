da28cecf9cda30a0803e3eda89d734baf0a13198
Andrés Devia <43211173+adeviat@users.noreply.github.com>
2025-12-01 15:09:57 +0100

    Se incluye maquetación de footer y estilos

--- src/app/components/organisms/footer/footer.component.ts
+++ src/app/components/organisms/footer/footer.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
+
 export class FooterComponent {
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,24 +1,21 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../components/organisms/header/header.component";
-import { HeroComponent } from "../../components/organisms/hero/hero.component";
-/* import { FiltersComponent } from '../../components/organisms/filters/filters.component';
-import { TripGridComponent } from '../../components/organisms/trip-grid/trip-grid.component';
-import { FooterComponent } from '../../components/organisms/footer/footer.component';*/
-
+import { HeaderComponent } from '../../components/organisms/header/header.component';
+import { HeroComponent } from '../../components/organisms/hero/hero.component';
+//import { FiltersComponent } from '../../components/organisms/filters/filters.component';
+//import { TripGridComponent } from '../../components/organisms/trip-grid/trip-grid.component';
+import { FooterComponent } from '../../components/organisms/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeaderComponent, 
+    HeaderComponent,
     HeroComponent,
-    /*FiltersComponent,
-    TripGridComponent,
-    FooterComponent,*/
+   // FiltersComponent,
+   // TripGridComponent,
+    FooterComponent,
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
